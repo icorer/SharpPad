92ba2a9878b835e8c09dfd15d75b3d131aa16426
corerman <admin@icorer.com>
2015-09-21 10:26:30 +0800

    First Upload

---
+++ CShapePad/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace CShapePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webControl1 = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // webControl1
+            // 
+            this.webControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(34)))), ((int)(((byte)(51)))));
+            this.webControl1.Location = new System.Drawing.Point(6, 36);
+            this.webControl1.Size = new System.Drawing.Size(466, 410);
+            this.webControl1.Source = new System.Uri("http://csharppad.com/", System.UriKind.Absolute);
+            this.webControl1.TabIndex = 0;
+            this.webControl1.DocumentReady += new Awesomium.Core.DocumentReadyEventHandler(this.webSource_Loader);
+            this.webControl1.LoadingFrame += new Awesomium.Core.LoadingFrameEventHandler(this.Web_Load);
+            this.webControl1.LoadingFrameComplete += new Awesomium.Core.FrameEventHandler(this.Web_Loaded);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(34)))), ((int)(((byte)(51)))));
+            this.BackLayout = false;
+            this.CanResize = false;
+            this.CaptionFont = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ClientSize = new System.Drawing.Size(478, 455);
+            this.Controls.Add(this.webControl1);
+            this.EffectBack = System.Drawing.SystemColors.ActiveCaption;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.InheritBack = true;
+            this.InnerBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(34)))), ((int)(((byte)(51)))));
+            this.InternalBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(34)))), ((int)(((byte)(51)))));
+            this.InternalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(34)))), ((int)(((byte)(51)))));
+            this.MaximizeBox = false;
+            this.MdiAutoScroll = false;
+            this.Name = "Form1";
+            this.Radius = 2;
+            this.SkinOpacity = 0.9D;
+            this.Text = "C Sharp Pad";
+            this.TitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public Awesomium.Windows.Forms.WebControl webControl1;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
---
+++ CShapePad/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CCWin;
+using System.Threading;
+
+
+namespace CShapePad
+{
+    public partial class Form1 : Skin_Metro
+    {
+        int status = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Web_Loaded(object sender, Awesomium.Core.FrameEventArgs e)
+        {
+          //  webControl1.Visible = true;
+          //  js();
+          //  status = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Loading...";
+            this.BackColor = Color.FromArgb(255, 0, 34, 51);
+           // this.InnerBorderColor = Color.FromArgb(255, 0, 34, 51);
+           // this.EffectBack = Color.FromArgb(255, 0, 34, 51);
+            webControl1.BackColor = Color.FromArgb(255, 0, 34, 51);
+        //    webControl1.LoadHTML("<html><body bgcolor=\"red\"></body></html>");
+           // webControl1.Source = new Uri("http://csharppad.com/");
+          //  webControl1.Enabled = true;
+            webControl1.Visible = false;
+
+        }
+
+
+        private void Web_Load(object sender, Awesomium.Core.LoadingFrameEventArgs e)
+        {
+            webControl1.BackColor = Color.FromArgb(255, 0, 34, 51);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          //  webControl1.Stop();
+          //  webControl1.
+            timer1.Stop();
+            if (status == 0)
+                this.Text = "Time Out";
+            js();
+        }
+
+
+        private void js() {
+            String js = "var obj=document.getElementsByClassName(\"pad-theme-dark\")[0];";
+            js += "document.body.removeChild(obj);";
+            js += "var obj2=document.getElementsByClassName(\"ui-dialog ui-widget ui-widget-content ui-corner-all ui-front ui-draggable ui-resizable\")[0];";
+            //  js+="obj2.style.marginTop=\"40px\"";
+
+           // js += "var js_tag=document.getElementsByTagName(\"script\");";
+           // js += "document.body.removeChild(js_tag[23]);";
+            webControl1.ExecuteJavascript(js);
+           
+           
+            if (status == 0)
+            {
+                status = 1;
+                webControl1.Reload(false);
+              //  webControl1.Visible = false;
+            }
+            else
+            {
+                webControl1.Visible = true;
+                this.Text = "C Sharp Pad";
+            }
+            
+          ///  MessageBox.Show(status.ToString());
+        }
+
+        private void webSource_Loader(object sender, Awesomium.Core.DocumentReadyEventArgs e)
+        {
+           // MessageBox.Show("ok");
+           
+           // MessageBox.Show(  );
+
+
+            js();
+            //webControl1.Reload(false);
+           // js();
+
+          //  webControl1.Visible = true;
+
+          //  webControl1
+            }
+    }
+}
